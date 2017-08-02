77a1bd7c24f404f7421358611609ce3010a2f08d
SavageBeast <sujithnair97@gmail.com>
2017-08-02 19:19:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,14 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_htm');
 submit.onclick = function () {
-    var names=['name1','name2','name3'];
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names=request.responseText;
+                names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++)
     {
@@ -31,4 +38,11 @@ submit.onclick = function () {
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET','http://sujithnair97.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
+    
 };
