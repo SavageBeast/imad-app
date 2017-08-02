5ec034e7c8a6421dc6fb45e7acb2133432b8a812
SavageBeast <sujithnair97@gmail.com>
2017-08-02 18:42:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,22 @@ function moveRight() {
     img.style.marginLeft= marginLeft+'px';
     
 }
+//counter
+var button=document.getElementById('counter');
+button.onclick = function () {
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function (){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.state===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('scount');
+    span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://sujithnair97.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
