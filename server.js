4e6b58e4878addbb0f1aaea6d9a4723012b58427
SavageBeast <sujithnair97@gmail.com>
2017-08-02 18:24:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,8 +72,11 @@ var htmltemplate=
 </html>`;
 return htmltemplate;
 }
-    
-    
+var couter=0;    
+app.get('/counter', function (req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+});   
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
