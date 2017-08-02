4fafa5b9a39f204d6777b424a1cd484108b4ba1d
SavageBeast <sujithnair97@gmail.com>
2017-08-02 18:14:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
 var img= document.getElementById('1');
 var marginLeft=0;
 img.onclick = function() {
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
     
 };
 function moveRight() {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     img.style.marginLeft= marginLeft+'px';
     
 }
