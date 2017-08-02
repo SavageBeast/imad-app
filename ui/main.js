cac505fd534640001a7a1cd4d9052846d411f503
SavageBeast <sujithnair97@gmail.com>
2017-08-02 19:21:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,10 +19,11 @@ button.onclick = function () {
     request.send(null);
 };
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_htm');
 submit.onclick = function () {
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if(request.readyState === XMLHttpRequest.DONE)
